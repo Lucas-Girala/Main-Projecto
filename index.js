cbc2215c27a3df29a0233461d3bcdc81a4252a6e
Lucas-Girala <81662343+Lucas-Girala@users.noreply.github.com>
2021-04-12 07:59:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function buss() {
     document.getElementById("teach").innerHTML = "Karen Bonifazi";
     document.getElementById("email").innerHTML = "Email";   
     document.getElementById("email").href = "mailto:kbonifazi@csa.edu.py";     
-    document.getElementById('zoom').innerHTML = "" 
-    document.getElementById('zoom').href = ""
+    document.getElementById('zoom').innerHTML = "Zoom" 
+    document.getElementById('zoom').href = "https://us04web.zoom.us/j/75563864657?pwd=S3JDYlIxSnM2OXVKSlJGSkF6Wnh2Zz09"
 }
 function PE() {
     document.body.style.backgroundColor = "blue";
