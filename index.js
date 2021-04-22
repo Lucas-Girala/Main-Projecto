fd9a3ec5db16b5c081ecc11c09b633eaa109bcb1
Lucas-Girala <81662343+Lucas-Girala@users.noreply.github.com>
2021-04-22 07:50:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function geol() {
     document.getElementById("email").href = "mailto:sulibarri@csa.edu.py";    
     document.getElementById('zoom').innerHTML = "Zoom" 
     document.getElementById('zoom').href = "https://us04web.zoom.us/j/78641521000?pwd=NjNCZ3dlcmc0emhBdjJidlRYVjczUT09"
+    
 }
 function math() {
     document.body.style.backgroundColor = "lightblue";
