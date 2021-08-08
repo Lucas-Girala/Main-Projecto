e0befcaa4a3dc4f2dd83231787bd431c0950aabf
Lucas-Girala <81662343+Lucas-Girala@users.noreply.github.com>
2021-08-08 01:14:34 -0400

    Update teacherlist.js

--- teacherlist.js
+++ teacherlist.js
@@ -2,87 +2,86 @@ var time=0
 function onClick() {
     time = time +30
     var random = Math.floor(Math.random()*11); 
-    document.getElementById("lol").src = "https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0;&amp;autoplay=1&amp;playlist=dQw4w9WgXcQ"
+    document.getElementById("lol").link = "https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0;&amp;autoplay=1&amp;playlist=dQw4w9WgXcQ"
     if (time>5000) {
       getOut()
     } 
     else if (time < 5000) {
         if (random == 0) {
-          document.body.style.backgroundColor = "red";
+          document.body.style.background = "red";
           document.body.style.color = "white"
           colorClock()
         }
         else if (random == 1) {
-          document.body.style.backgroundColor = "blue";
+          document.body.style.background = "blue";
           document.body.style.color = "white"
           colorClock()
         }
         else if (random == 2) {
-          document.body.style.backgroundColor = "green";
+          document.body.style.background = "green";
           document.body.style.color = "black"
           colorClock()
         }
           else if (random == 3) {
-          document.body.style.backgroundColor = "purple";
+          document.body.style.background = "purple";
           document.body.style.color = "white"
           colorClock()
         }
         else if (random == 4) {
-          document.body.style.backgroundColor = "cyan";
+          document.body.style.background = "cyan";
           document.body.style.color = "black"
           colorClock()
         }
         else if (random == 5) {
-          document.body.style.backgroundColor = "yellow";
+          document.body.style.background = "yellow";
           document.body.style.color = "black"
           colorClock()
           }
         else if (random == 6) {
-          document.body.style.backgroundColor = "grey";
+          document.body.style.background = "grey";
           document.body.style.color = "black"
           colorClock()
 
         }
         else if (random == 7) {
-          document.body.style.backgroundColor = "orange";
+          document.body.style.background = "orange";
           document.body.style.color = "black"
           colorClock()
         }
         else if (random == 8) {
-          document.body.style.backgroundColor = "white";
+          document.body.style.background = "white";
           document.body.style.color = "black"
           colorClock()
         }
         else if (random == 9) {
-          document.body.style.backgroundColor = "saddlebrown";
+          document.body.style.background = "saddlebrown";
           document.body.style.color = "white"
           colorClock()
         }
         else if (random == 10) {
-          document.body.style.backgroundColor = "black";
+          document.body.style.background = "black";
           document.body.style.color = "white"
           colorClock()
         }
     }
   }
   
-  function colorClock(){
+function colorClock(){
     var refresh=30
     mytime=setTimeout('onClick()', refresh)
     }
 
-  function getOut() {
-    document.body.style.backgroundColor = "black";
+function getOut() {
+    document.body.style.background = "linear-gradient(to right, #000000, #858585)";
     time = 0
   }
-
 
 var rightNow = new Date();
 var day = rightNow.getDay(); 
 var link;
 function esolcpe() {
   if (day==1) {
-    link = "https://us02web.zoom.us/j/82312897414" 
+    link = "https://zoom.us/j/97448214860"  
   } else if (day==3) {
     link = "https://us02web.zoom.us/j/87083885196"
   } else if (day==5) {
