56108e2d21aa2e3dca6e396f4c75e437b61c2394
Lucas-Girala <81662343+Lucas-Girala@users.noreply.github.com>
2021-08-26 09:10:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ function geom() {
     document.getElementById("renweb1").style.color = "black";
     document.getElementById("zoom1").style.color = "black";
     document.getElementById('email1').href = 'mailto:rportillo@csa.edu.py'
-    document.getElementById('zoom1').href = "https://us04web.zoom.us/j/4849009285?pwd=RjJMblRpSG9wbDFzODR2bzcxMmsxZz09" 
+    document.getElementById('zoom1').href = "https://us04web.zoom.us/j/2491783834?pwd=bjlZNmxjcHQrYXVMUlFRZXFBczhmdz09" 
     document.getElementById("link").style.color = "black";
 }
 function cont() {
